e5473731827f4a95597d22ec6120b26b5ca882e5
YeshNels <paleojosh@sbcglobal.net>
2022-07-12 17:39:23 -0700

    Rewrites quizfunctions to remove previous quizfunctions so that it proceeds in proper order. Still needs debugging, but reset function performs closer to normal expectations.

--- develop/assets/script.js
+++ develop/assets/script.js
@@ -13,14 +13,19 @@ var secondsLeft = 60;
 var isWin = false;
 
 function startQuiz(){
-    quizFirst();
     startTimer();
+    quizFirst();
 }
 function startTimer(){
+    //Putting the penalty checks in the timer function to try to impact the timer text count.
     var timerInterval = setInterval(function() {
         secondsLeft --;
-        timerEl.textContent= secondsLeft + " seconds remaining"
-        if (secondsLeft === 0) {
+        answerA.addEventListener("click", penaltyCheck);
+        answerB.addEventListener("click", penaltyCheck);
+        answerC.addEventListener("click", penaltyCheck);
+        answerD.addEventListener("click", penaltyCheck);
+        timerEl.textContent= secondsLeft + " seconds remaining";
+        if (secondsLeft <= 0) {
             clearInterval(timerInterval);
             youLose();
         }
@@ -40,89 +45,109 @@ function penaltyCheck(event){
     }
     else {
         secondsLeft -= 10;
+        console.log(secondsLeft);
         penaltyEl.textContent="Wrong!";
     }
 }
+//This doesn't work yet, need to rewrite the code to clear the buttons of all eventlisteners.
 function reset(){
+    isWin = false;
+    penaltyEl.textContent="";
     secondsLeft = 60;
     quizEl.style.visibility = "hidden";
+    //answerA.removeEventListener("click");
+    //answerB.removeEventListener("click");
+    //answerC.removeEventListener("click");
+    //answerD.removeEventListener("click");
 }
-answerA.addEventListener("click", penaltyCheck);
-answerB.addEventListener("click", penaltyCheck);
-answerC.addEventListener("click", penaltyCheck);
-answerD.addEventListener("click", penaltyCheck);
+
 function quizFirst(){
     secondsLeft = 60;
     quizEl.style.visibility = "visible";
     questionEl.textContent = "Which of these is not a main programming language?"
     answerA.textContent = "Console";
+    answerA.setAttribute("data-state", "correct");
     answerB.textContent = "Html";
+    answerB.setAttribute("data-state", "wrong");
     answerC.textContent = "Javascript"
+    answerC.setAttribute("data-state", "wrong");
     answerD.textContent = "Css";
+    answerD.setAttribute("data-state", "wrong");
     answerA.addEventListener("click", quizSecond);
-    answerB.addEventListener("click", penaltyFirst);
-    answerC.addEventListener("click", penaltyFirst);
-    answerD.addEventListener("click", penaltyFirst);
+    answerB.addEventListener("click", quizSecond);
+    answerC.addEventListener("click", quizSecond);
+    answerD.addEventListener("click", quizSecond);
 }
-//Currently, the event listeners for the penalty are still there, triggering for wrong answers even after that set of questions. Look into wiping the slate.
-function penaltyFirst(){
-    secondsLeft -= 10;
-    penaltyEl.textContent="Wrong!";
-    quizSecond();
-}
-//Look into consolidating the penalty functions, is it possible for an event to listen for clicking on a changing data attribute?
+//Currently seeing a bug where penaltycheck uses the updated data state rather than the one at the time of clicking. Need to make penaltycheck happen before any of the quiz functions.
+//After hitting the reset, all of the old event clickers are still there, sending the quiz back to the state where it was before reset once a button is clicked. Look into removing event listeners.
 function quizSecond(){
     questionEl.textContent = "Which of these is not a primitive variable?";
     answerA.textContent = "Boolean";
+    answerA.removeEventListener("click", quizFirst);
+    answerA.setAttribute("data-state", "wrong")
     answerB.textContent = "String";
+    answerB.removeEventListener("click", quizFirst);
+    answerB.setAttribute("data-state", "wrong")
     answerC.textContent = "Array";
+    answerC.removeEventListener("click", quizFirst);
+    answerC.setAttribute("data-state", "correct")
     answerD.textContent = "Number";
-    answerA.addEventListener("click", penaltySecond);
-    answerB.addEventListener("click", penaltySecond);
+    answerD.removeEventListener("click", quizFirst);
+    answerD.setAttribute("data-state", "wrong")
+    answerA.addEventListener("click", quizThird);
+    answerB.addEventListener("click", quizThird);
     answerC.addEventListener("click", quizThird);
-    answerD.addEventListener("click", penaltySecond);
+    answerD.addEventListener("click", quizThird);
 }
-function penaltySecond(){
-    secondsLeft -= 10;
-    penaltyEl.textContent="Wrong!";
-    quizThird();
-}
+
 function quizThird(){
     questionEl.textContent = "Which of these is a self-closing tag?";
     answerA.textContent = "img";
+    answerA.setAttribute("data-state", "correct");
     answerB.textContent = "p";
+    answerB.setAttribute("data-state", "wrong");
     answerC.textContent = "body";
+    answerC.setAttribute("data-state", "wrong");
     answerD.textContent = "div";
+    answerD.setAttribute("data-state", "wrong");
     answerA.addEventListener("click", quizFourth);
-    answerB.addEventListener("click", penaltyThird);
-    answerC.addEventListener("click", penaltyThird);
+    answerB.addEventListener("click", quizFourth);
+    answerC.addEventListener("click", quizFourth);
     answerD.addEventListener("click", quizFourth);
+    answerA.removeEventListener("click", quizSecond);
+    answerB.removeEventListener("click", quizSecond);
+    answerC.removeEventListener("click", quizSecond);
+    answerD.removeEventListener("click", quizSecond);
 }
-function penaltyThird(){
-    secondsLeft -=10;
-    penaltyEl.textContent="Wrong!";
-    quizFourth();
-}
+
 function quizFourth(){
     questionEl.textContent = "What is the outermost layer of the box?";
     answerA.textContent = "Padding";
+    answerA.setAttribute("data-state", "wrong");
     answerB.textContent = "Margin";
+    answerB.setAttribute("data-state", "correct");
     answerC.textContent = "Content";
+    answerC.setAttribute("data-state", "wrong");
     answerD.textContent = "Border";
-    answerA.addEventListener("click", penaltyFourth);
+    answerD.setAttribute("data-state", "wrong");
+    answerA.addEventListener("click", quizEnd);
     answerB.addEventListener("click", quizEnd);
-    answerC.addEventListener("click", penaltyFourth);
-    answerD.addEventListener("click", penaltyFourth);
+    answerC.addEventListener("click", quizEnd);
+    answerD.addEventListener("click", quizEnd);
+    answerA.removeEventListener("click", quizFourth);
+    answerB.removeEventListener("click", quizFourth);
+    answerC.removeEventListener("click", quizFourth);
+    answerD.removeEventListener("click", quizFourth);
 }
-function penaltyFourth(){
-    secondsLeft -=10;
-    penaltyEl.textContent="Wrong!";
-    quizEnd();
-}
+
 function quizEnd(){
     if (secondsLeft > 0){
         isWin = true;
     }
+    answerA.removeEventListener("click", quizFourth);
+    answerB.removeEventListener("click", quizFourth);
+    answerC.removeEventListener("click", quizFourth);
+    answerD.removeEventListener("click", quizFourth);
 }
 function youWin() {
     var initialWinner = prompt("Congratulations, you win! Please enter your initials for the scoreboard.")
